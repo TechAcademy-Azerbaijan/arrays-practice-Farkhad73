1e1b84b3a46d8d3763896567c6eddb8f0ec73df0
Farkhad73 <110536132+Farkhad73@users.noreply.github.com>
2022-09-02 10:28:34 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -4,6 +4,11 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+ let numbers = result.input.split(",")
+    let say = 0
+    for (let = if; i < numbers.length; i++){
+        ig (numbers[i]>numbers[i-1]&&numbers[i]>numbers[i+1]){
+            say = say + 1
+        }
+    }console.log(say);  
 });
